deb4faf12434006273518c3858a995d487b43a5c
Vitaliy <kv1993@me.com>
2024-06-15 23:05:58 +0800

    Added homework 10

---
+++ Next_lesson_10_homework/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    gender: "male",
+    name: { title: "Mr", first: "Magnus", last: "Jensen" },
+    location: {
+        street: { number: 5995, name: "Maribovej" },
+        city: "Pandrup",
+        state: "Midtjylland",
+        country: "Denmark",
+        postcode: 26227,
+        coordinates: { latitude: "-39.1719", longitude: "-142.7535" },
+        timezone: {
+        offset: "+4:00",
+        description: "Abu Dhabi, Muscat, Baku, Tbilisi",
+        },
+    },
+    email: "magnus.jensen@example.com",
+    login: {
+        uuid: "83a98972-aaa9-4d9d-a135-c8dbaefbef2e",
+        username: "browngoose787",
+        password: "anthony",
+        salt: "MPPL2LZj",
+        md5: "d57f64c903c80eb1a5315ef56aa13659",
+        sha1: "d453b23443dde1c4be6ad1bc3bc0de2e9860d948",
+        sha256:
+        "a227fb30ec0edf0f2156784cae2d104cac0240e41355a6803d1287e1a4b46bce",
+    },
+    dob: { date: "1974-03-16T02:32:29.805Z", age: 50 },
+    registered: { date: "2018-10-15T22:44:22.970Z", age: 5 },
+    phone: "68978918",
+    cell: "03404968",
+    id: { name: "CPR", value: "150374-8103" },
+    picture: {
+        large: "https://randomuser.me/api/portraits/men/72.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/72.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/72.jpg",
+    },
+    nat: "DK",
+  },
+];
